c28be09bed2ad00d210383271a91e284b17b09f3
marcebenitez2 <marcebenitez0607@gmail.com>
2024-02-20 14:36:44 -0300

    chore: Agregar Scss a nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+import path from "path";
+
 const nextConfig = {
     reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(process.cwd(), "styles")],
+    },
 };
 
 export default nextConfig;
