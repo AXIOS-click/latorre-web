a1c6bc5884d08aae3bf7b2dda1962b49a3865f68
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-02-23 01:10:55 -0500

    feat/refactor: agrega nuevos cambios para petry

--- src/assets/images/imageProvider.ts
+++ src/assets/images/imageProvider.ts
@@ -1,5 +1,6 @@
 import PetryWhiteLogoSvg from "./icons/PetryWhiteLogo.svg";
-import PetryTemporal from "./icons/petry.png";
+import PetryTemporal from "./icons/Petry.svg";
+import Latorre from "./icons/Latorre.svg";
 import BurgerMenu from "./icons/BurgerMenu.svg";
 
-export { PetryWhiteLogoSvg, PetryTemporal, BurgerMenu };
+export { PetryWhiteLogoSvg, PetryTemporal, BurgerMenu, Latorre };
--- src/modules/Home/components/PetryCenter/index.tsx
+++ src/modules/Home/components/PetryCenter/index.tsx
@@ -1,20 +1,30 @@
 "use client";
-import { PetryTemporal } from "@/assets/images/imageProvider";
+import { Latorre, PetryTemporal } from "@/assets/images/imageProvider";
 import Image from "next/image";
 import { useState } from "react";
 
 const PetryCenter = () => {
     const [animationEndend, setAnimationEndend] = useState(false);
     return (
-        <Image
-            src={PetryTemporal}
-            alt="Pintura Petry"
-            width={384}
-            height={384}
-            priority
-            className={`${animationEndend ? "petry_center" : "scale-down-center"}`}
-            onAnimationEnd={() => setAnimationEndend(!animationEndend)}
-        />
+        <div className="absolute w-full justify-center flex flex-col items-center justify-items-center">
+            <Image
+                src={PetryTemporal}
+                alt="Pintura Petry"
+                width={384}
+                height={384}
+                priority
+                className={`${animationEndend ? "petry_center" : "scale-down-center"}`}
+                onAnimationEnd={() => setAnimationEndend(!animationEndend)}
+            />
+            <Image
+                src={Latorre}
+                alt="Pintura Petry"
+                width={384}
+                height={384}
+                className={`fade-in ${animationEndend ? "show" : ""} mt-3`}
+                priority
+            />
+        </div>
     );
 };
 
