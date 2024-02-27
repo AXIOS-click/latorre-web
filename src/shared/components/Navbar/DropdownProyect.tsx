a0334e02b629c7baf28d9e38b057d0e597a92744
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-02-26 23:36:20 -0500

    chore: cambia las configuraciones

--- src/shared/components/Navbar/DropdownProyect.tsx
+++ src/shared/components/Navbar/DropdownProyect.tsx
@@ -7,26 +7,25 @@ export const DropdownProyect = ({ classNameProp }: { classNameProp: string }) =>
     const [openDropdown, setOpenDropdown] = useState(false);
 
     return (
-        <div>
+        <div onMouseEnter={() => setOpenDropdown(!openDropdown)} onMouseLeave={() => setOpenDropdown(!openDropdown)}>
             <button
                 data-fc-type="dropdown"
                 type="button"
                 data-fc-offset={0}
                 data-fc-trigger="hover"
-                onClick={() => setOpenDropdown(!openDropdown)}
-                className={`text-lg hover:text-xl transition-all ${classNameProp}`}
+                className={`text-2xl hover:text-3xl transition-all ${classNameProp}`}
             >
                 Proyectos
             </button>
             <div className={` ${openDropdown ? "block" : "hidden"} z-50 p-2 absolute`}>
                 <Link
-                    className="flex items-center py-2 px-3 rounded-md  hover:bg-latorre-red"
+                    className="flex items-center py-2 px-3 rounded-md hover:bg-latorre-red"
                     href='/proyects/sculture'
                 >
                     Esculturas
                 </Link>
                 <Link
-                    className="flex items-center py-2 px-3 rounded-md   hover:bg-latorre-red"
+                    className="flex items-center py-2 px-3 rounded-md hover:bg-latorre-red"
                     href='/proyects/paintings'
                 >
                     Pinturas
