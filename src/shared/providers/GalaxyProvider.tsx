45d346de54a7cd33e60996d30629552f35bb1d3d
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-02-29 13:04:44 -0500

    fix: arregla errores en el canvas

--- src/shared/providers/GalaxyProvider.tsx
+++ src/shared/providers/GalaxyProvider.tsx
@@ -6,44 +6,43 @@ import { AdditiveBlending } from "three";
 import { GalaxyStore } from "./store";
 
 const GalaxyRenderer = () => {
-    const parameters = {
-        count: 10000,
-        stars: 9600,
-        starColor: "#1b3984",
-        size: 0.001,
-        radius: 10,
-        branches: 8,
-        spin: 5,
-        randomness: 2,
-        randomnessPower: 10,
-        insideColor: "#a930ff",
-        outsideColor: "#fff",
-    };
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    let geometry = new THREE.BufferGeometry();
-    const textureLoader = new THREE.TextureLoader();
-    const shape = textureLoader.load("/particleShape/1.png");
-    let bgStarsGeometry: any = null;
-    let bgStarsMaterial: any = null;
-    let bgStars: any = null;
-
-    let material = new THREE.PointsMaterial({
-        color: "white",
-        size: parameters.size,
-        depthWrite: false,
-        sizeAttenuation: true,
-        blending: AdditiveBlending,
-        vertexColors: true,
-        transparent: true,
-        alphaMap: shape,
-    });
-    let points = new THREE.Points(geometry, material);
-    const sizes = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-    };
-
     useEffect(() => {
+        const parameters = {
+            count: 10000,
+            stars: 9600,
+            starColor: "#1b3984",
+            size: 0.001,
+            radius: 10,
+            branches: 8,
+            spin: 5,
+            randomness: 2,
+            randomnessPower: 10,
+            insideColor: "#a930ff",
+            outsideColor: "#fff",
+        };
+        let geometry = new THREE.BufferGeometry();
+        const textureLoader = new THREE.TextureLoader();
+        const shape = textureLoader.load("/particleShape/1.png");
+        let bgStarsGeometry: any = null;
+        let bgStarsMaterial: any = null;
+        let bgStars: any = null;
+
+        let material = new THREE.PointsMaterial({
+            color: "white",
+            size: parameters.size,
+            depthWrite: false,
+            sizeAttenuation: true,
+            blending: AdditiveBlending,
+            vertexColors: true,
+            transparent: true,
+            alphaMap: shape,
+        });
+        let points = new THREE.Points(geometry, material);
+        const sizes = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
         const canvas = canvasRef.current;
         const scene = new THREE.Scene();
 
@@ -67,7 +66,6 @@ const GalaxyRenderer = () => {
             bgStarsGeometry.setAttribute("position", new THREE.BufferAttribute(bgStarsPositions, 3));
 
             bgStarsMaterial = new THREE.PointsMaterial({
-                color: "white",
                 size: parameters.size,
                 depthWrite: false,
                 sizeAttenuation: true,
@@ -75,9 +73,9 @@ const GalaxyRenderer = () => {
                 color: parameters.starColor,
                 transparent: true,
                 alphaMap: shape,
-            });
+            }) as unknown as any;
 
-            bgStars = new THREE.Points(bgStarsGeometry, bgStarsMaterial);
+            bgStars = new THREE.Points(bgStarsGeometry, bgStarsMaterial) as unknown as any;
 
             scene.add(bgStars);
         };
@@ -141,7 +139,7 @@ const GalaxyRenderer = () => {
         camera.position.z = 8;
         scene.add(camera);
         const renderer = new THREE.WebGLRenderer({
-            canvas: canvas,
+            canvas: canvas as HTMLCanvasElement | OffscreenCanvas | undefined,
             alpha: true,
         });
         renderer.setSize(sizes.width, sizes.height);
