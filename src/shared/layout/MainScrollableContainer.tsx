e2a733ca80404a064efaeb491d97d59fb70fe422
marcebenitez2 <marcebenitez0607@gmail.com>
2024-03-05 14:02:46 -0300

    fix: Correccion zIndex ScrolleableContainer

--- src/shared/layout/MainScrollableContainer.tsx
+++ src/shared/layout/MainScrollableContainer.tsx
@@ -9,7 +9,7 @@ export const MainScrollableContainer: FC<IMainScrollableContainer> = ({ children
         <main className="h-screen max-h-screen overflow-hidden">
             <div className="container mx-auto h-full">
                 <section className="block h-full">
-                    <div className="block h-full overflow-y-auto z-50 relative">{children}</div>
+                    <div className="block h-full overflow-y-auto z-40 relative">{children}</div>
                 </section>
             </div>
         </main>
