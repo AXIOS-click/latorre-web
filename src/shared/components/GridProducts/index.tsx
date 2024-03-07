21c04cb9bcf9d4577164a0c4b3b1ada78c82ddb3
marcebenitez2 <marcebenitez0607@gmail.com>
2024-03-07 16:18:55 -0300

    fix: Cambio en la logica de renderizado de imagenes de productos

--- src/shared/components/GridProducts/index.tsx
+++ src/shared/components/GridProducts/index.tsx
@@ -1,14 +1,17 @@
 import { IStrapiScultureAndPaints } from "@/shared/services/strapi/Escultura/domain/Escultura";
+import { imgProvider } from "@/shared/utils/imgProvider";
 
 export const GridProducts = ({ arrayProductos }: { arrayProductos: IStrapiScultureAndPaints[] }) => (
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-12">
         {arrayProductos.map(({ attributes }, index) => {
             const { ImagenPrincipal } = attributes;
+            const { URL_IMAGES } = imgProvider();
             return (
                 <div key={`${index + 1}`} className="w-full h-full inline-block overflow-hidden">
                     <img
-                        src={ImagenPrincipal?.data?.attributes.url}
+                        src={URL_IMAGES + ImagenPrincipal?.data?.attributes.url}
                         className="w-full h-full hover:scale-110 transition-transform duration-500"
+                        alt="imagen de escultura"
                     />
                 </div>
             );
