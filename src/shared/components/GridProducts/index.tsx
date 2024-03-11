86cefca760b45a234c21f6138eca886aa8a5bec8
marcebenitez2 <marcebenitez0607@gmail.com>
2024-03-11 17:49:15 -0300

    feat: cambio en los estilos y logica de redireccionamiento de producto

--- src/shared/components/GridProducts/index.tsx
+++ src/shared/components/GridProducts/index.tsx
@@ -1,19 +1,20 @@
 "use client";
 import { IStrapiScultureAndPaints } from "@/shared/services/strapi/Escultura/domain/Escultura";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export const GridProducts = ({ arrayProductos }: { arrayProductos: IStrapiScultureAndPaints[] }) => {
+    const actualRoute = usePathname();
     return (
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-12">
-            {arrayProductos.map(({ attributes }, index) => {
-                const { ImagenPrincipal } = attributes;
+            {arrayProductos.map((producto, index) => {
+                const { id } = producto;
+                const { attributes } = producto;
+                const { ImagenPrincipal } = producto.attributes;
                 return (
-                    <Link href={attributes.slug} key={index}>
-                        <ImageWhitTitle
-                            imagen={ImagenPrincipal?.data?.attributes.url}
-                            titulo={attributes.Titulo}
-                        />
+                    <Link href={actualRoute + "/" + id} key={index}>
+                        <ImageWithTitle imagen={ImagenPrincipal?.data?.attributes.url} titulo={attributes.Titulo} />
                     </Link>
                 );
             })}
@@ -21,21 +22,18 @@ export const GridProducts = ({ arrayProductos }: { arrayProductos: IStrapiScultu
     );
 };
 
-const ImageWhitTitle = ({ imagen, titulo }: { imagen: string; titulo: string }) => {
+const ImageWithTitle = ({ imagen, titulo }: { imagen: string; titulo: string }) => {
     const [viewTitle, setViewTitle] = useState(false);
     return (
         <div
-            className="w-full h-full relative"
             onMouseEnter={() => setViewTitle(true)}
             onMouseLeave={() => setViewTitle(false)}
+            className="relative overflow-hidden"
         >
-            <div className="w-full h-96 inline-block overflow-hidden">
-                <img
-                    src={imagen}
-                    className="w-full h-full transition-transform duration-500 hover:scale-110"
-                    alt="imagen de escultura"
-                />
-            </div>
+            <div
+                style={{ backgroundImage: `url(${imagen})` }}
+                className="h-[600px] w-full bg-cover bg-no-repeat bg-center hover:scale-110 transition-all duration-300 rounded-md"
+            />
             <div
                 className={`absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-center py-4 opacity-0 transition-opacity duration-300 ${viewTitle ? "opacity-100" : "opacity-0"}`}
             >
